6ed99908a5c827f479c962e2cc97f138c5446010
ChuckGross <chuck.a.gross@outlook.com>
2016-11-12 21:34:30 -0500

    Add files via upload

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yesNo = "y";
+            while (yesNo == "y" || yesNo == "Y")
+            {
+                Console.Write("Please enter a word: ");
+                string word = Console.ReadLine();
+                char[] charArray = word.ToCharArray();
+                Array.Reverse(charArray);
+                string reverseWord = new string(charArray);
+
+                if (word == reverseWord)
+                {
+                    Console.WriteLine(word + " is a palindrome!");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, " + word + " is not a palindrome.");
+                    Console.WriteLine();
+                }
+                Console.Write("Do you want to check another word? (y/n): ");
+                yesNo = Console.ReadLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
