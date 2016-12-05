3b3361438dad6ea39c8615760fa65eeceb1ac0ed
ChuckGross <chuck.a.gross@outlook.com>
2016-12-05 07:37:48 -0500

    Add files via upload

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a string: ");
+            string s = Console.ReadLine();
+            
+            Console.WriteLine(s.Reverse());
+        }
+    }
+}
