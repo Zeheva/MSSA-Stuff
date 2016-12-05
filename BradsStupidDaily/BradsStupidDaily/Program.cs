58b89967e5a9eac33ea5480ca7d2852fe131ad4f
ChuckGross <chuck.a.gross@outlook.com>
2016-12-05 07:35:18 -0500

    Add files via upload

---
+++ BradsStupidDaily/BradsStupidDaily/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BradsStupidDaily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yn = "y";
+            while (yn == "y")
+            {
+                Console.WriteLine("Enter an integer: ");
+                int num = int.Parse(Console.ReadLine());
+                for (int i = num; i >= 0; i--)
+                {
+                    for (int x = i; x > 0; x--)
+                    {
+                        Console.Write(num);
+                    }
+                    Console.WriteLine();
+                }
+                Console.Write("Do you want to try another integer? (y/n): ");
+                yn = Console.ReadLine();
+            }          
+        }
+    }
+}
---
+++ IsogramTester/IsogramTester/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsogramTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yesNo = "y";
+            while (yesNo == "y")
+            {
+                Console.WriteLine("Enter a string to test: ");
+                string s = Console.ReadLine();
+                char[] inputArray = new char[s.Length];
+                inputArray = s.ToCharArray();
+
+                int test = 0;
+                int position = 0;
+                foreach (var item in inputArray)
+                {
+                    for (int i = position + 1; i < s.Length; i++)
+                        if (test == 0)
+                        {
+                            {
+                                if (item == inputArray[i])
+                                {
+                                    test = 1;
+                                }
+
+                                if (test == 1)
+                                {
+                                    Console.WriteLine("{0} is not an isogram.", s);
+                                }
+                            }
+                        }
+                    position++;
+                }
+
+                if (test == 0)
+                {
+                    Console.WriteLine("{s} is an Isogram.", s);
+                }
+
+                Console.Write("Do you want to test another string? (y/n): ");
+                yesNo = Console.ReadLine();
+            }           
+        }
+    }
+}
