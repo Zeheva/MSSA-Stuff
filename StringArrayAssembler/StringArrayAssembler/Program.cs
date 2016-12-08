d18d6d8e1751813132d08fba10a2d8dd7c6745d3
ChuckGross <chuck.a.gross@outlook.com>
2016-12-08 09:01:57 -0500

    Add files via upload

---
+++ StringArrayAssembler/StringArrayAssembler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringArrayAssembler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = new string[] { "a", "c", "e", "g", "h" };
+            string[] array2 = new string[] { "b", "d", "f", "i", "j" };
+            List<string> list3 = new List<string>(array1.Concat<string>(array2));
+
+            foreach (var item in list3)
+            {
+                Console.Write(item);               
+            }
+
+            Console.WriteLine();
+            list3.Sort();
+
+            foreach (var item in list3)
+            {
+                Console.Write(item);
+            }
+            Console.ReadLine();            
+        }        
+    }
+}
