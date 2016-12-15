c54c0c8a343078241f21defe16bce299a3d3a785
ChuckGross <chuck.a.gross@outlook.com>
2016-12-15 10:02:00 -0500

    Add files via upload
    Code snippet to demonstrate implementation of the exclusive-OR operator to 'toggle' a Boolean value.

---
+++ ToggleTest/ToggleTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToggleTest
+{
+    class Program
+    {
+        /*Intent of code snippet is to demonstrate use of the 
+        exclusive-OR operator to 'toggle' a binary condition.*/
+        static void Main(string[] args)
+        {
+            bool toggle = false;
+            for (int i = 0; i < 10; i++)
+            {
+                if (toggle != true)
+                {
+                    Console.WriteLine("Toggle is off.");
+                }
+                else
+                {
+                    Console.WriteLine("Toggle is on.");
+                }
+                toggle ^= true;
+                
+            }          
+        }
+    }
+}
