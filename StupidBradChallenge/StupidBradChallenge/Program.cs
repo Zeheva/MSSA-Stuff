29da22e7594244eaaf87a9768bc0901b1b279c4f
ChuckGross <chuck.a.gross@outlook.com>
2017-01-24 21:29:21 -0500

    Add files via upload

---
+++ StupidBradChallenge/StupidBradChallenge/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupidBradChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            if (stringTest(Console.ReadLine()) == true)
+                {
+                Console.WriteLine("String has repeating characters.");
+                }
+            else Console.WriteLine("All characters are unique.");
+        }
+
+        public static bool stringTest(string str)
+        {
+            bool b = false;
+            int n = 1;
+            int l = str.Length;
+            foreach (var item in str)
+            {               
+                
+                for (int i = n; i < l; i++)
+                {                    
+                    if (item == str[i])
+                        b = true;                   
+                }
+                n++;                
+            }
+            return b;
+        }
+    }
+}
